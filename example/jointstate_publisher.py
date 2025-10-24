abc3977c2f016b5f04bb2376677509608d7e33d4
Heewon Lee <7dlgmldnjs7@gmail.com>
2025-10-24 17:59:39 +0900

    Add joint state publisher and subscriber implementations
    Signed-off-by: Heewon Lee <7dlgmldnjs7@gmail.com>

---
+++ example/jointstate_publisher.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+#
+# Copyright 2025 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Taehyeong Kim, Heewon Lee
+
+
+import time
+import math
+
+from robotis_dds_python.idl.sensor_msgs.msg import JointState_
+from robotis_dds_python.idl.std_msgs.msg import Header_
+from robotis_dds_python.idl.builtin_interfaces.msg import Time_
+from robotis_dds_python.tools.topic_manager import TopicManager
+
+
+# Use the utility function to create the writer
+topic_manager = TopicManager()
+writer = topic_manager.topic_writer(topic_name='/joint_states', topic_type=JointState_ )
+
+
+def create_joint_state(t: float) -> JointState_:
+    joint_names = ["j1", "j2", "j3"]
+    positions = []
+    velocities = []
+    efforts = []
+
+    for i in range(3):
+        positions.append(math.sin(t + i))
+        velocities.append(math.cos(t + i))
+        efforts.append(0.0)
+
+    now = time.time()
+    sec = int(now)
+    nsec = int((now - sec) * 1e9)
+
+    header = Header_(stamp=Time_(sec=sec, nanosec=nsec), frame_id="base_link")
+
+    return JointState_(
+        header=header,
+        name=joint_names,
+        position=positions,
+        velocity=velocities,
+        effort=efforts
+    )
+
+
+t = 0.0
+
+try:
+    while True:
+        t += 0.1
+        msg = create_joint_state(t)
+        writer.write(msg)
+        print(f"Published {len(msg.name)} joints")
+        time.sleep(1.0)
+except KeyboardInterrupt:
+    print("\nPublisher stopped.")
