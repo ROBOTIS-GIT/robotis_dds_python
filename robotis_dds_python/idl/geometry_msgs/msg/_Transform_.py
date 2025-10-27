afbc0a7f39b99a47327ac2e4c2e82a2975e2fc14
Heewon Lee <7dlgmldnjs7@gmail.com>
2025-10-27 15:43:49 +0900

    Refactor Transform_ and TransformStamped_ type annotations for consistency
    Signed-off-by: Heewon Lee <7dlgmldnjs7@gmail.com>

--- robotis_dds_python/idl/geometry_msgs/msg/_Transform_.py
+++ robotis_dds_python/idl/geometry_msgs/msg/_Transform_.py
@@ -18,11 +18,11 @@ import cyclonedds.idl.types as types
 # import geometry_msgs
 
 from robotis_dds_python.idl.std_msgs.msg import Header_
-
+from robotis_dds_python.idl.geometry_msgs.msg import Vector3_, Quaternion_
 
 @dataclass
 @annotate.final
 @annotate.autoid("sequential")
 class Transform_(idl.IdlStruct, typename="geometry_msgs.msg.dds_.Transform_"):
-    translation: 'robotis_dds_python.idl.geometry_msgs.msg.Vector3_'
-    rotation: 'robotis_dds_python.idl.geometry_msgs.msg.Quaternion_'
+    translation: 'Vector3_'
+    rotation: 'Quaternion_'
